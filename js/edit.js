e50d9044d1c095f69223d4bf36a15fa81e788b8a
gaby <morengab@gmail.com>
2013-11-16 12:48:13 -0500

    can add shortcuts with basic form

--- js/edit.js
+++ js/edit.js
@@ -7,14 +7,27 @@ $j(function() {
         $j("form#create_shortcut").slideDown();
     });
 
-
     $j('form#create_shortcut').submit(function() {
         var formInput = $j(this).serialize();
-        $j.post(
-            $j(this).attr('action'),
-            formInput, 
-            function(data, textStatus, jqXHR){
-                console.log(data);
+        // $j.post(
+        //     $j(this).attr('action'),
+        //     formInput, 
+        //     function(data, textStatus, jqXHR){
+        //         console.log(data);
+        //         // clear inputs
+        //         $j("input#shortcut_name").val("");
+        //         $j("input#shortcut_code").val("");
+        //         $j("input#shortcut_image_url").val("");
+
+        //         // hide form
+        //         $j("form#create_shortcut").slideUp();
+        //     });
+
+        $j.ajax({
+            url: $j(this).attr('action'),
+            data: formInput,
+            success: function(data, textStatus, jqXHR) {
+                console.log(textStatus);
                 // clear inputs
                 $j("input#shortcut_name").val("");
                 $j("input#shortcut_code").val("");
@@ -22,7 +35,9 @@ $j(function() {
 
                 // hide form
                 $j("form#create_shortcut").slideUp();
-            });
+            }
+        });
+
         return false;   
     });
 }); 
